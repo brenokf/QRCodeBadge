b24d5a3029a58938f18b447fe0803c7a166d8851
brenokf <breno-kf@hotmail.com>
2021-02-27 01:36:07 -0400

    modification slint of position

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
-/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-unresolved */
 import { Component } from '@angular/core';
+
+/* eslint-disable import/prefer-default-export */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  
+
 }
