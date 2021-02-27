706ca032a712957642ac6635dc2b4e9580ee17e0
brenokf <breno-kf@hotmail.com>
2021-02-27 01:30:35 -0400

    add routes home,generate,contacts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,34 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/prefer-default-export */
+import { ContactsComponent } from './views/contacts/contacts.component';
+import { GenerateComponent } from './views/generate/generate.component';
+import { HomeComponent } from './views/home/home.component';
 /* eslint-disable import/order */
 /* eslint-disable import/order */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent,
+    data: { animation: 'HomePage' },
+  },
+
+  {
+    path: 'generate',
+    component: GenerateComponent,
+    data: { animation: 'GeneratePage' },
+  },
+
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
